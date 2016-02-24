03944c1e60ef483c84463687ec700befcb7156bb
Gustavo Avena <gustavoavena@Gustavos-MBP.wireless.yale.internal>
2016-02-24 17:02:26 -0500

    startig...

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var path    = require("path");
+
+
+// app.use(express.static('static'));
+app.use(express.static(__dirname + '/static'));
+
+
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World!');
+// });
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
